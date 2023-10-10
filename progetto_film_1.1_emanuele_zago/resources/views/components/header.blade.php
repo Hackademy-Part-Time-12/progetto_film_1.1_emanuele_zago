ef2ac06ab3005e7ec3901fe0118ba312aeaf4dfc
EmanueleZago <lelepoci@gmail.com>
2023-10-10 20:21:15 +0200

    aggiunto un po di roba

---
+++ progetto_film_1.1_emanuele_zago/resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<div class="container my-5 shadow">
+    <div class="row">
+        <h1 class="text-size 3 text-center">Sito Film</h1>
+    </div>
+</div>
---
+++ progetto_film_1.1_emanuele_zago/resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources\css\app.css', 'resources\js\app.js'])
+    <title>Film</title>
+</head>
+
+<body>
+<x-navbar />
+<x-header />
+
+    <div class="min-vh-100">
+        {{$slot}}
+    </div>
+
+<x-footer />
+</body>
+
+</html>
